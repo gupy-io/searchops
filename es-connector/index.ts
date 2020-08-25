90be0f319438ca2224b5a448a7d78df092319841
Victor Villas <mail@victor.villas>
2020-08-25 19:08:45 -0300

    Copy es-connector module as sibling package

---
+++ es-connector/index.ts
@@ -0,0 +1,39 @@
+import awsSdk from 'aws-sdk';
+import { Client, events } from '@elastic/elasticsearch';
+import { Logger } from 'winston';
+import { AwsSignedConnection, UnsignedConnection } from './aws';
+
+let client: Client;
+
+interface ElasticsearchConfig {
+  elasticHost: string;
+  elasticPort: number;
+  elasticProtocol: string;
+}
+
+interface DatabaseInput{
+  elasticConfig: ElasticsearchConfig;
+  logger: Logger;
+}
+
+export const createElasticsearch = (
+  { elasticConfig, logger }: DatabaseInput,
+): Client | null => {
+  if (!client) {
+    const host = elasticConfig.elasticHost || 'localhost';
+    const port = elasticConfig.elasticPort || 9200;
+    const protocol = elasticConfig.elasticProtocol || 'http';
+
+    client = new Client({
+      Connection: awsSdk.config.credentials ? AwsSignedConnection : UnsignedConnection,
+      node: `${protocol}://${host}:${port}`,
+    });
+    client.on(events.RESPONSE, (_error, result) => {
+      logger.silly(JSON.stringify(result, null, 2));
+      if (result && result.warnings) {
+        logger.warn(result.warnings);
+      }
+    });
+  }
+  return client;
+};
