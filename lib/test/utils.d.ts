6d707c498506ba38c8347d547f0906f7e9bdd606
Victor Villas <mail@victor.villas>
2020-09-15 11:32:55 -0300

    Remove test files from dist build

--- lib/test/utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "@elastic/elasticsearch";
-import { Config } from "../service";
-export declare function getRandomSnakeCase(): string;
-export declare function getTestClient(): Client;
-export declare function getRandomConfig(): Config;
-export declare type RecursivePartial<T> = {
-    [P in keyof T]?: RecursivePartial<T[P]>;
-};
