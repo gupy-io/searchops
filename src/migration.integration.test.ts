cecbd783f9376338595645cb585d55ee4c56db0e
Victor Villas <mail@victor.villas>
2020-09-17 19:05:34 -0300

    Purge old migration tests and place new skeleton

--- src/migration.integration.test.ts
+++ src/migration.integration.test.ts
@@ -1,133 +1,34 @@
-/* eslint-disable @typescript-eslint/unbound-method */
 import { context, test } from "./test/language";
-import { getTestClient, getRandomSnakeCase } from "./test/utils";
-import { Config } from "./service";
-import { IndexManager } from "./migration";
 
-context("IndexManager", () => {
+context("Index Migrations", () => {
   test("Creating a new index", (_) => {
     _.givenTheIndex().wasNotCreated();
     _.whenTheManager().performsMigration();
     _.thenTheIndex().shouldExist();
   });
-
-  const esClient = getTestClient();
-  const esConfig: Config = {
-    index: getRandomSnakeCase(),
-    alias: getRandomSnakeCase(),
-    dtype: getRandomSnakeCase(),
-    settings: {
-      number_of_shards: "1",
-      number_of_replicas: "1",
-      refresh_interval: "1ms",
-    },
-    mappings: { properties: { code: { type: "text" } } },
-  };
-
-  const manager = new IndexManager({ esClient, esConfig });
-
-  beforeEach(() => {
-    jest.spyOn(esClient, "reindex");
-    jest.spyOn(esClient, "updateByQuery");
-    jest.spyOn(esClient.indices, "create");
-    jest.spyOn(esClient.indices, "delete");
-    jest.spyOn(esClient.indices, "putMapping");
-    jest.spyOn(esClient.indices, "putSettings");
-    jest.spyOn(esClient.indices, "updateAliases");
+  test("Checking an existing index", (_) => {
+    _.givenTheIndex().wasCreated();
+    _.whenTheManager().performsMigration();
+    _.thenTheIndex().shouldExist();
   });
-  afterEach(async () => {
-    try {
-      await manager.deleteIndex();
-    } catch {
-      // shhh don't worry
-    }
-    jest.restoreAllMocks();
+  test("Updating dynamic index settings", (_) => {
+    _.givenTheIndex().wasCreated();
+    _.whenTheManager().performsMigration();
+    _.thenTheIndex().shouldExist();
   });
-
-  describe("when aliased index does not exist", () => {
-    beforeEach(async () => {
-      expect(await manager.existsIndex()).toBeFalsy();
-      expect(await manager.existsAlias()).toBeFalsy();
-    });
-    it("should create aliased index with settings and mappings", async () => {
-      await manager.migrate();
-
-      expect(await manager.existsIndex()).toBeTruthy();
-      expect(await manager.existsAlias()).toBeTruthy();
-      expect(await manager.getSettings()).toEqual(esConfig.settings);
-      expect(await manager.getMappings()).toEqual(esConfig.mappings);
-    });
+  test("Updating dynamic index settings", (_) => {
+    _.givenTheIndex().wasCreated();
+    _.whenTheManager().performsMigration();
+    _.thenTheIndex().shouldExist();
   });
-
-  describe("when aliased index already exists", () => {
-    let initialVersion: string;
-
-    beforeEach(async () => {
-      await manager.migrate();
-      initialVersion = await manager.getVersion();
-      await esClient.index({
-        index: esConfig.index,
-        body: { code: "123/456" },
-        refresh: "wait_for",
-      });
-    });
-
-    describe("when settings and mappings are still the same", () => {
-      beforeEach(async () => {
-        expect(await manager.getSettings()).toEqual(esConfig.settings);
-        expect(await manager.getMappings()).toEqual(esConfig.mappings);
-        jest.resetAllMocks();
-      });
-      it("should perform no maintenance", async () => {
-        await manager.migrate();
-
-        expect(await manager.getVersion()).toEqual(initialVersion);
-        expect(esClient.reindex).not.toHaveBeenCalled();
-        expect(esClient.updateByQuery).not.toHaveBeenCalled();
-        expect(esClient.indices.create).not.toHaveBeenCalled();
-        expect(esClient.indices.delete).not.toHaveBeenCalled();
-        expect(esClient.indices.putMapping).not.toHaveBeenCalled();
-        expect(esClient.indices.putSettings).not.toHaveBeenCalled();
-        expect(esClient.indices.updateAliases).not.toHaveBeenCalled();
-      });
-    });
-
-    describe("when dynamic mappings have changed", () => {
-      beforeEach((): void => {
-        esConfig.mappings = {
-          properties: {
-            ...esConfig.mappings.properties,
-            code: { type: "text", fields: { keyword: { type: "keyword" } } },
-          },
-        };
-      });
-      afterEach(() => {
-        jest.restoreAllMocks();
-      });
-      it("should update mappings without recreating index", async () => {
-        await manager.migrate();
-        expect(await manager.getVersion()).toEqual(initialVersion);
-        expect(await manager.getSettings()).toEqual(esConfig.settings);
-        expect(await manager.getMappings()).toEqual(esConfig.mappings);
-      });
-    });
-
-    describe("when dynamic settings have changed", () => {
-      beforeEach(() => {
-        esConfig.settings = {
-          ...esConfig.settings,
-          refresh_interval: "2ms",
-        };
-      });
-      afterEach(() => {
-        jest.resetAllMocks();
-      });
-      it("should update settings without recreating index", async () => {
-        await manager.migrate();
-        expect(await manager.getVersion()).toEqual(initialVersion);
-        expect(await manager.getSettings()).toEqual(esConfig.settings);
-        expect(await manager.getMappings()).toEqual(esConfig.mappings);
-      });
-    });
+  test("Updating dynamic index mappings", (_) => {
+    _.givenTheIndex().wasCreated();
+    _.whenTheManager().performsMigration();
+    _.thenTheIndex().shouldExist();
+  });
+  test("Updating static index mappings", (_) => {
+    _.givenTheIndex().wasCreated();
+    _.whenTheManager().performsMigration();
+    _.thenTheIndex().shouldExist();
   });
 });
