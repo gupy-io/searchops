c9c111d3eed11ac4d2ab544ce173b26a3497e797
TiagoEspanha <tiago.espanha@gmail.com>
2021-05-06 19:05:28 -0300

    IN-79 - Remove client caching (#252)
    * Remove client caching
    
    * Run lint and build
    
    * Add instructions of how to start elasticsearch container and how to deploy a new project version
    
    * Improve readme
    
    * Update README.md
    
    Co-authored-by: Victor Villas <mail@victor.villas>
    
    * Update README.md
    
    Co-authored-by: Victor Villas <mail@victor.villas>
    
    Co-authored-by: Victor Villas <mail@victor.villas>

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# searchops
+# Searchops
+
+### Iniciar o container que contém o Elasticsearch
+```bash
+docker-compose up -d
+```
+
+### Processo de desenvolvimento
+- Rodar os comandos:
+```bash
+npm test
+npm run lint
+npm run build
+```
+
 
--- src/es-connector/index.ts
+++ src/es-connector/index.ts
@@ -3,8 +3,6 @@ import { Client } from "@elastic/elasticsearch";
 import type { WinstonLogger } from "../typings/winston";
 import { AwsSignedConnection, UnsignedConnection } from "./aws";
 
-let client: Client;
-
 interface ElasticsearchConfig {
   elasticHost: string;
   elasticPort: number;
@@ -20,23 +18,21 @@ export const createElasticsearch = ({
   elasticConfig,
   logger,
 }: DatabaseInput): Client => {
-  if (!client) {
-    const host = elasticConfig.elasticHost || "localhost";
-    const port = elasticConfig.elasticPort || 9200;
-    const protocol = elasticConfig.elasticProtocol || "http";
+  const host = elasticConfig.elasticHost || "localhost";
+  const port = elasticConfig.elasticPort || 9200;
+  const protocol = elasticConfig.elasticProtocol || "http";
 
-    client = new Client({
-      Connection: awsSdk.config.credentials
-        ? AwsSignedConnection
-        : UnsignedConnection,
-      node: `${protocol}://${host}:${port}`,
-    });
-    client.on("response", (_error, result) => {
-      logger.silly(JSON.stringify(result, null, 2));
-      if (result && result.warnings) {
-        logger.warn(result.warnings);
-      }
-    });
-  }
+  const client = new Client({
+    Connection: awsSdk.config.credentials
+      ? AwsSignedConnection
+      : UnsignedConnection,
+    node: `${protocol}://${host}:${port}`,
+  });
+  client.on("response", (_error, result) => {
+    logger.silly(JSON.stringify(result, null, 2));
+    if (result && result.warnings) {
+      logger.warn(result.warnings);
+    }
+  });
   return client;
 };
