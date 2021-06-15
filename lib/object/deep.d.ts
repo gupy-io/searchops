1e90f135106728b9f6d4f31cf36e1f4ff7cc49e7
Daniel Tamai <daniel.tamai@gmail.com>
2021-06-15 13:01:06 -0300

    chore: Remove generated files (#304)
    Fix #50

--- lib/object/deep.d.ts
+++
@@ -1,8 +0,0 @@
-declare type Value = unknown;
-declare type TreeLike = {
-    [key: string]: TreeLike | Value;
-};
-declare type ValueOrTree = Value | TreeLike;
-export declare function deepEqual(obj1: unknown, obj2: unknown): boolean;
-export declare function deepPatch(obj1: ValueOrTree, obj2: ValueOrTree): ValueOrTree;
-export {};
